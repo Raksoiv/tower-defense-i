030b7d8132e7c9106bbc3ed88908aa46ccb6678f
Oscar Rencoret <o.rencoret@gmail.com>
2022-06-12 13:23:41 -0400

    Tower stats

--- Resources/Towers/TowerData.gd
+++ Resources/Towers/TowerData.gd
@@ -2,7 +2,8 @@ extends Resource
 class_name TowerData
 
 
+export(int) var bullet_velocity
+export(int) var cost
+export(int) var damage
 export(int) var fire_range
 export(float) var fire_rate
-export(int) var damage
-export(int) var bullet_velocity
